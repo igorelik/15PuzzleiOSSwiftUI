4aa201da09249789a4b4c45af4ce5d0fa5303d28
Igor Gorelik <gorelik.igor@gmail.com>
2021-01-04 11:51:36 +1100

    Initial Commit

---
+++ DragDropTest/DragDropTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropTestApp.swift
+//  DragDropTest
+//
+//  Created by Igor Gorelik on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DragDropTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
